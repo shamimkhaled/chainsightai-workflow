d65205b09821ad6ac47f473fc942bd0c6e851d0a
shamimkhaled <i.amkhaled89@gmail.com>
2025-10-08 13:47:25 +0600

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    {
+      pattern: /^(bg|border|text)-(blue|purple|pink|green|orange|gray|yellow)-(50|100|200|500|600|700|800|900)$/,
+    },
+  ],
   theme: {
     extend: {},
   },
